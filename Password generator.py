53230bdd9088e34ada5b3163291ab9bec122bc7e
SydG04 <sgilmore@students.pccc.edu>
2023-05-18 16:30:34 -0400

    Don't mind this

---
+++ Password generator.py
@@ -0,0 +1,17 @@
+def main():
+
+
+main()
+
+
+
+'''
+1. Start the program.
+2. Ask the user how long they want their password to be.
+3. Read the user's input for the desired length of the password.
+4. Generate a password string that is of the specified length, by:
+   a. Creating an empty string variable to hold the password.
+   b. Using a loop to generate random characters until the desired length is reached.
+   c. For each iteration of the loop, generate a random character (using the random module) and append it to the password string.
+5. Print the generated password to the console.
+6. End the program.
