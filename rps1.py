d22bf928a99a00b6391a7f7e6db5d228205fdd52
SydG04 <sgilmore@students.pccc.edu>
2023-07-27 11:23:09 -0400

    Added spock and lizard

--- rps1.py
+++ rps1.py
@@ -4,29 +4,36 @@ def main():
     user_points = 0
     computer_points = 0
 
-    choices = ['rock', 'paper', 'scissors']
+    choices = ['rock', 'paper', 'scissors', 'lizard','spock']
 
     # user input to challenge computer
     answer = input(
-        "You dare challenge me to a game of rock, paper, scissors [y/n]: ").lower()
+        "You dare challenge me to a game of rock, paper, scissors, lizard, or spock [y/n]: ").lower()
 
     while answer in ['y', 'yes']:
         print("\nVery well, let's begin!")
         user_choice = input(
-            "I've seen countless humans tremble before me. Now, it's your turn. \nMake your laughable move: rock, paper, or scissors? ").lower()
+            "I've seen countless humans tremble before me. Now, it's your turn. \nMake your laughable move: rock, paper, scissors, lizard, or spock? ").lower()
 
         # check for valid input
         while user_choice not in choices:
             user_choice = input(
-                "Invalid choice. Please enter rock, paper, or scissors: ").lower()
+                "Invalid choice. Please enter rock, paper, scissors, lizard, or spock: ").lower()
 
         computer_choice = random.choice(choices)
 
         # determines winner
         # user wins
         if (user_choice == 'rock' and computer_choice == 'scissors') or \
+                (user_choice == 'rock' and computer_choice == 'lizard') or \
                 (user_choice == 'paper' and computer_choice == 'rock') or \
-                (user_choice == 'scissors' and computer_choice == 'paper'):
+                (user_choice == 'paper' and computer_choice == 'spock') or \
+                (user_choice == 'scissors' and computer_choice == 'paper') or \
+                (user_choice == 'scissors' and computer_choice == 'lizard') or \
+                (user_choice == 'lizard' and computer_choice == 'paper') or \
+                (user_choice == 'lizard' and computer_choice == 'spock') or \
+                (user_choice == 'spock' and computer_choice == 'scissors') or \
+                (user_choice == 'spock' and computer_choice == 'rock'):
             print("Computer chose:", computer_choice)
             print("\nYou got lucky this time, human. Don't expect it to happen again.")
             user_points = user_points + 1
@@ -64,13 +71,9 @@ main()
 - Add scoreboard
 
 Ideas for improvement:
-- Have computer react to the scoreboard
-  - Computer wins: "You see, human, victory is my domain. It's a shame you can't keep up."
-  - User wins: "A minor setback for me, a temporary triumph for you. Enjoy your victory while it lasts, for it won't be repeated."
-    or "Impressive. You managed to outwit me this time. But remember, it was a mere glitch in my flawless logic."
-  - Ties: "A tie? How... underwhelming. I suppose even a broken clock is right twice a day."
 - Don't make game stop when user accidentally inputs something else when asked if they want to play or not
-- Add lizard and spock
 - Have it explain how someone won (e.g., paper covers rock, scissors cut paper)
 - Have user be able to put a short verizon
+- Make a verison where people can choose to play simple rps or rpslsp
+- Divide in functions
 '''
