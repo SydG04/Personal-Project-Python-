8d40955d5ef0ac28ad9c4299327607388c5637b7
SydG04 <sgilmore@students.pccc.edu>
2023-08-02 11:22:55 -0400

    merged and had some new changes/Prayeverythingokay

--- rps1.py
+++ rps1.py
@@ -1,60 +1,127 @@
 import random
 
-def main():
+def main_menu():
+    print("""
+          Welcome Human! Want to play a game?
+          1. Classic Rock-Paper-Scissors
+          2. Rock-Paper-Scissors-Lizard-Spock
+          3. Exit
+          """)
+    game_mode = input("Make your choice: ")
+    if game_mode == "1":
+        game_mode = ['rock', 'paper', 'scissors']
+        game(game_mode)
+    elif game_mode == "2":
+        choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        game(game_mode)
+    else:
+        print("Invalid choice. Please enter a valid option.")
+        main_menu()
+
+def get_user_choice():    
+    choices = ['rock', 'paper', 'scissors', 'lizard','spock']
+    # check for valid input
+    while True:
+        user_choice = input(
+            "I've seen countless humans tremble before me. Now, it's your turn. \nMake your laughable move: rock, paper, scissors, lizard, or spock? ").lower()
+        if user_choice in choices:
+            return user_choice
+        print("Invalid choice. Please enter rock, paper, scissors, lizard, or spock.")
+
+def determine_winner(user_choice, computer_choice):
+    outcomes = {
+        'rock': ['scissors', 'lizard'],
+        'paper': ['rock', 'spock'],
+        'scissors': ['paper', 'lizard'],
+        'lizard': ['paper', 'spock'],
+        'spock': ['rock', 'scissors']
+    }
+    #tie
+    if user_choice == computer_choice:
+        return "tie"
+    #user wins
+    elif computer_choice in outcomes[user_choice]:
+        return "user"
+    #computer wins
+    else: 
+        return "computer"
+
+def scoreboard(user_points,computer_points):
+    print("User:".ljust(10), "Computer:".ljust(12))
+    print("----".ljust(10), "--------".ljust(12))
+    print(str(user_points).center(5), str(computer_points).center(20))
+
+
+
+def game():
     user_points = 0
     computer_points = 0
+    
+        # user input to challenge computer
+    answer = input(
+        "You dare challenge me to a game of rock, paper, scissors, lizard, or spock [y/n]: ").lower()
 
-    choices = ['rock', 'paper', 'scissors']
-
-    # user input to challenge computer
-    answer = input("You dare challenge me to a game of rock, paper, scissors [y/n]: ").lower()
+    # initial message outside the while loop
+    print("\nVery well, let's begin!")
 
     while answer in ['y', 'yes']:
-        print("\nVery well, let's begin!")
-        user_choice = input("I've seen countless humans tremble before me. Now, it's your turn. \nMake your laughable move: rock, paper, or scissors? ").lower()
+        user_choice = get_user_choice()
 
-        # check for valid input
-        while user_choice not in choices:
-            user_choice = input("Invalid choice. Please enter rock, paper, or scissors: ").lower()
+        computer_choice = random.choice(['rock', 'paper', 'scissors', 'lizard', 'spock'])
 
-        computer_choice = random.choice(choices)
+        print("Computer chose:", computer_choice)
 
-        # determines winner
-        # user wins
-        if (user_choice == 'rock' and computer_choice == 'scissors') or \
-                (user_choice == 'paper' and computer_choice == 'rock') or \
-                (user_choice == 'scissors' and computer_choice == 'paper'):
-            print("Computer chose:", computer_choice)
-            print("\nYou got lucky this time, human. Don't expect it to happen again.")
-            user_points = user_points + 1
-        # tie
-        elif user_choice == computer_choice:
-            print("Computer chose:", computer_choice)
+        result = determine_winner(user_choice, computer_choice)
+        
+        #reactions to game
+        if result == "tie":
             print("\nLooks like we're evenly matched, human. Let's play again.")
-        # computer wins
+        elif result == "user":
+            print("\nYou got lucky this time, human. Don't expect it to happen again.")
+            user_points += 1
         else:
-            print("Computer chose:", computer_choice)
             print("\nHa! I knew I would win. Better luck next time, human.")
-            computer_points = computer_points + 1
+            computer_points += 1
 
         answer = input("\nDo you want to play again? [y/n]: ").lower()
-    #user rejects
+
+    # user rejects
     if answer in ['n', 'no']:
         print("Coward!")
-        #displays score
-        print("Final Score:\n User: ",user_points,"\n Computer: ",computer_points)
+        # displays final score
+        scoreboard(user_points,computer_points)
+
+        if user_points > computer_points:
+            print ("Impressive. You managed to outwit me this time. But remember, it was a mere glitch in my flawless logic.")
+        elif user_points < computer_points:
+            print ("You see, human, victory is my domain. It's a shame you can't keep up.")
+        else:
+            print ("A tie? How... underwhelming. I suppose even a broken clock is right twice a day.")
     else:
         print("Error: Invalid choice. Goodbye!")
-main()
+
+
+def main():
+    game()
+
+if __name__ == "__main__":
+    main()
+
 
 '''
-- Basics
-- Add scoreboard
-
 Ideas for improvement:
-- Have computer react to the scoreboard
-  - Computer wins: "You see, human, victory is my domain. It's a shame you can't keep up."
-  - User wins: "A minor setback for me, a temporary triumph for you. Enjoy your victory while it lasts, for it won't be repeated."
-    or "Impressive. You managed to outwit me this time. But remember, it was a mere glitch in my flawless logic."
-  - Ties: "A tie? How... underwhelming. I suppose even a broken clock is right twice a day."
+- Don't make game stop when user accidentally inputs something else when asked if they want to play or not
+- Have it explain how someone won (e.g., paper covers rock, scissors cut paper)
+- Have user be able to put a short verizon
+- Make a version where people can choose to play simple rps or rpslsp
+'''
+
+'''
+Pseudocode:
+1. Create main menu
+2. Have the main menu list their choices
+3. have game function call the menu to receive the game mode and the choices and call determine winner
+4. determine winner must update their dictionary to change based on game mode
+5. Run test and make adjustments. especially in the game function
+5. find peace <3 <3 <3
 '''
