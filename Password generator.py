2eb9d22309acbfab135b74be128407bd7fd3f5f1
SydG04 <sgilmore@students.pccc.edu>
2023-08-18 21:51:56 -0400

    simple password generator

--- Password generator.py
+++ Password generator.py
@@ -1,5 +1,22 @@
-def main():
+import secrets
+import string
 
+def main():
+   #required length of password defined by user
+   req_length = int(input("How long do you want your password to be? "))
+
+   letters = string.ascii_letters #upper and lower case letters
+   digits = string.digits #digits from 0-9
+   special_characters = string.punctuation #special characters
+
+   #concatenate the above string constants
+   alphabet = letters + digits + special_characters
+
+   pwd = ''
+   for i in range (req_length):
+      pwd +=''.join(secrets.choice(alphabet))
+
+   print(pwd)
 
 main()
 
@@ -15,3 +32,4 @@ main()
    c. For each iteration of the loop, generate a random character (using the random module) and append it to the password string.
 5. Print the generated password to the console.
 6. End the program.
+'''
